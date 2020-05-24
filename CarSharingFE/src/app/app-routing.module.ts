50b7156f998d353d858949bba7f83afb4697104f
Alexandru Voinea <alexandru.voinea@mindit.io>
2020-05-24 19:55:58 +0300

    Final application

--- CarSharingFE/src/app/app-routing.module.ts
+++ CarSharingFE/src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AddComponent } from './add/add.component';
 import {EditComponent} from './edit/edit.component';
+import { AssistanceComponent } from './assistance/assistance.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'add', component: AddComponent },
-  {path: 'edit',component:EditComponent},
-  { path: '**', component: HomeComponent },
+  { path: '', component: HomeComponent},
+  { path: 'add', component: AddComponent},
+  {path: 'edit',component: EditComponent},
+  {path: 'contact',component: ContactComponent},
+  {path: 'assistance',component: AssistanceComponent},
+  { path: '**', component: HomeComponent},
   
-
-
 ];
 
 @NgModule({
